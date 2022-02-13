971565a51bb7c3452746134c9eec53c74f248832
autumnly1007 <autumnly1007@gmail.com>
2022-02-13 12:12:58 +0900

    handle state change on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../button/button";
 import ImageFileInput from "../image_file_input/image_file_input";
 import styles from "./card_edit_form.module.css";
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
   const { name, company, title, email, message, theme, fileName, fileURL } =
     card;
-  const onSubmit = () => {};
+
+  const onSubmit = () => {
+    deleteCard(card);
+  };
+
+  const onChange = (event) => {
+    if (event.currentTarget == null) {
+      return;
+    }
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} ref={formRef}>
       <input
         className={styles.input}
         type="text"
         name="name"
         defaultValue={name}
+        ref={nameRef}
+        onChange={onChange}
       />
       <input
         className={styles.input}
         type="text"
         name="company"
         defaultValue={company}
+        ref={companyRef}
+        onChange={onChange}
       />
-      <select className={styles.select} name="theme" defaultValue={theme}>
+      <select
+        className={styles.select}
+        name="theme"
+        defaultValue={theme}
+        ref={themeRef}
+        onChange={onChange}
+      >
         <option value="light">light</option>
         <option value="dark">dark</option>
         <option value="colorful">colorful</option>
@@ -31,17 +64,23 @@ const CardEditForm = ({ card }) => {
         type="text"
         name="title"
         defaultValue={title}
+        ref={titleRef}
+        onChange={onChange}
       />
       <input
         className={styles.input}
         type="text"
         name="email"
         defaultValue={email}
+        ref={emailRef}
+        onChange={onChange}
       />
       <textarea
         className={styles.textarea}
         name="message"
         defaultValue={message}
+        ref={messageRef}
+        onChange={onChange}
       ></textarea>
       <div className={styles.fileInput}>
         <ImageFileInput />
